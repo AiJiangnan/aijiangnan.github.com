c2217c597f838b259477ad2761748c818d386708
Jiangnan Ai <904629998@qq.com>
2016-11-25 20:28:59 +0800

    百度统计

--- javascript/menu.js
+++ javascript/menu.js
@@ -13,3 +13,11 @@ var _hmt = _hmt || [];
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?3af5aea5e529dd6efeb462126425248a";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
