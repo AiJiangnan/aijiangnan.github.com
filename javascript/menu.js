e6609f9b8991e2af5b2b8519a7a60b9b8c287f84
Jiangnan Ai <904629998@qq.com>
2016-11-25 20:26:58 +0800

    百度统计来访人数

--- javascript/menu.js
+++ javascript/menu.js
@@ -5,3 +5,11 @@ function menu_show(id){
 function menu_hidden(id){
 	document.getElementById(id).style.display = "none";
 }
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?3055db4375a485cde1b0a416587c137d";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
