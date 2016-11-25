1c8eec8de393eb174d6426c59857b39a2e9bb69c
Jiangnan Ai <904629998@qq.com>
2016-11-25 20:46:33 +0800

    百度统计

--- javascript/menu.js
+++ javascript/menu.js
@@ -9,14 +9,6 @@ function menu_hidden(id){
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?3055db4375a485cde1b0a416587c137d";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
   hm.src = "https://hm.baidu.com/hm.js?3af5aea5e529dd6efeb462126425248a";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
