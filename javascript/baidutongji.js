bf4022598fabc8e0c8240d07359d8aa458ab24a6
Jiangnan Ai <904629998@qq.com>
2016-12-11 16:30:09 +0800

    computer完成

--- javascript/baidutongji.js
+++ javascript/baidutongji.js
@@ -1,7 +1,45 @@
+// XML read
+function XMLPath(xmlpath) {
+  if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp = new XMLHttpRequest();
+  } else {// code for IE6, IE5
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.open("GET", xmlpath, false);
+  xmlhttp.send();
+  xmlDoc = xmlhttp.responseXML;
+}
+
+
+// 百度统计
 var _hmt = _hmt || [];
-(function() {
+(function () {
   var hm = document.createElement("script");
   hm.src = "https://hm.baidu.com/hm.js?3af5aea5e529dd6efeb462126425248a";
-  var s = document.getElementsByTagName("script")[0]; 
+  var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
+
+// Menu
+function showMenu() {
+  var menu = document.getElementById('menu');
+  menu.style.display = 'block';
+  menu.style.position = 'fixed';
+  if (i < 1) {
+    menu.style.opacity = i;
+    i += 0.1;
+    setTimeout('showMenu()', speed);
+  }
+}
+function hiddenMenu() {
+  var menu = document.getElementById('menu');
+  if (i >= 0) {
+    menu.style.opacity = i;
+    i -= 0.1;
+    setTimeout('hiddenMenu()', speed);
+  }
+  if (i >= 0 && i <= 0.1) {
+    menu.style.display = 'none';
+    menu.style.position = 'absolute';
+  }
+}
